bd90c433a9899d4e40080337e8517f703506ab1a
3th-Enjay <enjayjerey@gmail.com>
2025-06-30 13:20:09 +0100

    some pictures

--- app/data/shopCategories.tsx
+++ app/data/shopCategories.tsx
@@ -7,6 +7,7 @@ export type ColorImage = {
 export type ColorVariant = {
   colorName: string;
   images: ColorImage[];
+  price?: number; // Optional price for specific color variants
 };
 
 export type CategoryImage = {
@@ -23,143 +24,114 @@ const defaultSizes = ["S", "M", "L", "XL"];
 export const baseCategoryImages: Record<string, CategoryImage[]> = {
   Dresses: [
     {
-      id: "floral-dress-d",
-      name: "Floral Dress",
-      price: 59.99,
+      id: "column-dress",
+      name: "Column Dress",
+      price: 115000,
       description: "A vibrant floral dress perfect for spring and summer outings. Light and breezy with a flattering fit.",
       sizes: defaultSizes,
       colorVariants: [
-        {
-          colorName: "Red",
+         {
+          colorName: "Pink",
           images: [
-            { src: "/Rectangle-3.jpg", alt: "Floral Dress Red Front" },
-            { src: "/Rectangle-3.jpg", alt: "Floral Dress Red Back" },
+            { src: "/coloumn-Stretch-Woven2.jpg", alt: "Column Stretch Pink Front" },
+            { src: "/coloumn-Stretch-Woven1.jpg", alt: "Floral Dress Blue Back" },
+            { src: "/coloumn-Stretch-Woven3.jpg", alt: "Column Stretch Pink Side" },
+          ],
+        },
+        {
+          colorName: "Black",
+          images: [
+            { src: "/coloumn-Cotton-Adire.jpg", alt: "column dress black  Front" },
+            { src: "/coloumn-Cotton-Adire2.jpg", alt: "column dress black  side" },
+            { src: "/coloumn-Cotton-Adire3.jpg", alt: "column dress black back" },
+          ],
+        }
+       
+      ],
+    },
+    {
+      id: "clarinet-dress",
+      name: "Clarinet Dress",
+      price: 125000,
+      description: "Comfortable and stylish casual maxi dress, perfect for everyday wear or a relaxed evening out.",
+      sizes: defaultSizes,
+      colorVariants: [
+        {
+          colorName: "white",
+          images: [
+            { src: "/clarinet-Cotton-Adire.jpg", alt: "clarinet cotton adire white Front" },
+            { src: "/clarinet-Cotton-Adire2.jpg", alt: "clarinet cotton white Side" },
+            { src: "/clarinet-Cotton-Adire3.jpg", alt: "clarinet cotton adire white Back" },
           ],
         },
         {
           colorName: "Blue",
           images: [
-            { src: "/Rectangle-2.jpg", alt: "Floral Dress Blue Front" },
-            { src: "/Rectangle-2.jpg", alt: "Floral Dress Blue Back" },
+            { src: "/clarinet-Stretch-Woven.jpg", alt: "clarinet stretch Front" },
+            { src: "/clarinet-Stretch-Woven2.jpg", alt: "clarinet stretch side" },
+            { src: "/clarinet-Stretch-Woven3.jpg", alt: "clarinet stretch back" },
           ],
-        },
-      ],
-    },
-    {
-      id: "casual-maxi-d",
-      name: "Casual Maxi",
-      price: 49.99,
-      description: "Comfortable and stylish casual maxi dress, perfect for everyday wear or a relaxed evening out.",
-      sizes: defaultSizes,
-      colorVariants: [
-        {
-          colorName: "Black",
-          images: [
-            { src: "/Rectangle.jpg", alt: "Casual Maxi Black Front" },
-            { src: "/Rectangle-2.jpg", alt: "Casual Maxi Black Side" },
-          ],
-        },
-        {
-          colorName: "White",
-          images: [
-            { src: "/Rectangle.jpg", alt: "Casual Maxi White Front" },
-            { src: "/Rectangle-2.jpg", alt: "Casual Maxi White Back" },
-          ],
+          price: 115000,
         },
       ],
     },
   ],
   Skirts: [
     {
-      id: "floral-dress-s",
-      name: "Floral Dress",
-      price: 39.99,
+      id: "flute-skirt",
+      name: "Flute Skirt",
+      price: 90000,
       description: "A charming floral skirt that pairs well with any top, designed for a feminine and fresh look.",
       sizes: defaultSizes,
       colorVariants: [
         {
           colorName: "Pink",
           images: [
-            { src: "/Rectangle-3.jpg", alt: "Floral Skirt Pink Front" },
-            { src: "/Rectangle-2.jpg", alt: "Floral Skirt Pink Back" },
-          ],
-        },
-        {
-          colorName: "Green",
-          images: [
-            { src: "/Rectangle.jpg", alt: "Floral Skirt Green Front" },
-            { src: "/Rectangle-2.jpg", alt: "Floral Skirt Green Side" },
+            { src: "/flute-Cotton-Adire.png", alt: "Flute Skirt Pink Front" },
+            { src: "/flute-Cotton-Adire2.png", alt: "Flute Skirt Pink Back" },
           ],
         },
       ],
     },
-    {
-      id: "casual-maxi-s",
-      name: "Casual Maxi",
-      price: 44.99,
+   
+  ],
+  Tops: [
+ {
+      id: "loop-top",
+      name: "Loop Top",
+      price: 60000,
       description: "A versatile casual maxi skirt, perfect for both casual days and dressier occasions.",
       sizes: defaultSizes,
       colorVariants: [
         {
-          colorName: "Navy",
+          colorName: "Black",
           images: [
-            { src: "/Rectangle.jpg", alt: "Casual Maxi Navy Front" },
-            { src: "/Rectangle-2.jpg", alt: "Casual Maxi Navy Side" },
-          ],
-        },
-        {
-          colorName: "Gray",
-          images: [
-            { src: "/Rectangle-2.jpg", alt: "Casual Maxi Gray Front" },
-            { src: "/Rectangle-2.jpg", alt: "Casual Maxi Gray Back" },
-          ],
-        },
-      ],
-    },
-  ],
-  Tops: [
-    {
-      id: "floral-dress-t",
-      name: "Floral Dress",
-      price: 29.99,
-      description: "Lightweight floral top that adds a splash of color and style to your casual wardrobe.",
-      sizes: defaultSizes,
-      colorVariants: [
-        {
-          colorName: "Yellow",
-          images: [
-            { src: "/Rectangle-3.jpg", alt: "Floral Top Yellow Front" },
-            { src: "/Rectangle-2.jpg", alt: "Floral Top Yellow Back" },
-          ],
-        },
-        {
-          colorName: "Orange",
-          images: [
-            { src: "/Rectangle.jpg", alt: "Floral Top Orange Front" },
-            { src: "/Rectangle-2.jpg", alt: "Floral Top Orange Side" },
+            { src: "/loop-Cotton-Adire.png", alt: "loop-top Front" },
+            { src: "/loop-Cotton-Adire2.png", alt: "loop-top back" },
+            { src: "/loop-Cotton-Adire3.png", alt: "loop-top side" },
           ],
         },
       ],
     },
     {
-      id: "casual-maxi-t",
-      name: "Casual Maxi",
-      price: 34.99,
+      id: "step-top",
+      name: "Step Top",
+      price: 70000,
       description: "Simple and chic casual top that pairs well with jeans or skirts for an effortless look.",
       sizes: defaultSizes,
       colorVariants: [
         {
-          colorName: "White",
+          colorName: "blue",
           images: [
-            { src: "/Rectangle.jpg", alt: "Casual Top White Front" },
-            { src: "/Rectangle-2", alt: "Casual Top White Back" },
+            { src: "/step-Cotton-Adire-Blue.png", alt: "Casual Top White Front" },
+            { src: "/step-Cotton-Adire-Blue2.jpg", alt: "Casual Top White Back" },
           ],
         },
         {
-          colorName: "Black",
+          colorName: "pink",
           images: [
-            { src: "/Rectangle.jpg", alt: "Casual Top Black Front" },
-            { src: "/Rectangle-2.jpg", alt: "Casual Top Black Side" },
+            { src: "/step-Stretch-Woven.jpg", alt: "Casual Top Black Front" },
+
           ],
         },
       ],
